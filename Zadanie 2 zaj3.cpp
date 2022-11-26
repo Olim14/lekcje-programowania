1bd67a500de942fa172756c67e5c764118a0ef07
Olim14 <118050423+Olim14@users.noreply.github.com>
2022-11-26 11:44:36 +0100

    Add files via upload

---
+++ Zadanie 2 zaj3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int  licznik = 10;
+    int nr = 1 ;
+    while (licznik >0)
+        {
+    cout << nr<<". " <<  " Hello world " << endl;
+   licznik --;
+   nr ++;
+   }
+}
