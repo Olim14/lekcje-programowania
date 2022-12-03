27a99d0aef2f292966d2890f02aa7aa2806d3e0b
Olim14 <118050423+Olim14@users.noreply.github.com>
2022-12-03 12:04:13 +0100

    Add files via upload

---
+++ Zadanie domowe 2 zajcia 3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    //Napisz program, który wczytuje kolejne liczby ca³kowite od u¿ytkownika i je ze sob¹ mno¿y, dopóki iloczyn nie przekroczy 10
+    int iloczyn=1;
+    int liczba;
+
+    while (iloczyn <= 100){
+        cin>> liczba;
+        iloczyn*=liczba;
+    cout<<"iloczyn: "<<iloczyn<< endl;
+    }
+}
