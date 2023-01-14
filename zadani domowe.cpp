0a3920da37cebfd21472a0705767dee01119f604
Olim14 <118050423+Olim14@users.noreply.github.com>
2023-01-14 12:03:15 +0100

    Add files via upload

---
+++ zadani domowe.cpp
@@ -0,0 +1,26 @@
+/*Napisz program, ktory policzy sume wszystkich wielokrotnosci liczb 3 lub 5 mniejszych od 1000.
+ Program ma za zadanie zwróciæ SUMÊ wszystkich liczb naturalnych mniejszych od 1000, które s¹ podzielne przez 3 lub 5.*/
+
+#include<iostream>
+using namespace std;
+
+   bool czypodzielnaprzez3lub5 (int x)
+   {    if (x%3==0|| x%5==0)
+        return true;
+       else return false;}
+
+int main()
+
+{    int suma = 0;
+      for(int i= 3; i<1000; i ++)
+    {
+    if( czypodzielnaprzez3lub5( i ) == true)
+    {
+        cout<<i<< endl;
+        suma = suma + i;
+        }
+
+    }
+    cout<< suma;
+    }
+
